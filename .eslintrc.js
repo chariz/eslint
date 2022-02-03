864d3259e0cc5c8bc438e81b70a2542a46509841
Adam Demasi <adam@hbang.com.au>
2022-02-03 17:17:16 +1030

    feat: Add more rules from react-app and unicorn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/prefer-module */
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
 	env: {
@@ -9,7 +10,9 @@ module.exports = {
 	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/eslint-recommended",
-		"preact"
+		"react-app",
+		"preact",
+		"plugin:unicorn/all"
 	],
 
 	globals: {
@@ -28,61 +31,57 @@ module.exports = {
 		sourceType: "module"
 	},
 
-	plugins: ["react", "@typescript-eslint"],
+	plugins: ["react", "unicorn", "@typescript-eslint"],
 
 	rules: {
 		"array-callback-return": "error",
 		"comma-dangle": ["error", "never"],
-
-		eqeqeq: [
-			"error",
-			"always",
-			{
-				null: "ignore"
-			}
-		],
-		indent: [
-			"error",
-			"tab",
-			{
-				SwitchCase: 1,
-				offsetTernaryExpressions: true
-			}
-		],
-		quotes: [
-			"error",
-			"double",
-			{
-				allowTemplateLiterals: true
-			}
-		],
-		semi: [
-			"error",
-			"always",
-			{
-				omitLastInOneLineBlock: true
-			}
-		],
-
-		"newline-per-chained-call": [
-			"error",
-			{
-				ignoreChainWithDepth: 1
-			}
-		],
-		"object-shorthand": ["error", "always"],
-		"prefer-const": "off",
-		"no-var": "error",
-		"no-unused-vars": "off",
-		"prefer-template": "error",
-		"no-useless-return": "error",
+		eqeqeq: ["error", "always", { null: "ignore" }],
+		indent: ["error", "tab", { SwitchCase: 1, offsetTernaryExpressions: true }],
+		"newline-per-chained-call": ["error", { ignoreChainWithDepth: 1 }],
 		"no-implicit-coercion": "error",
 		"no-spaced-func": "off",
+		"no-unused-vars": "off",
+		"no-useless-return": "error",
+		"no-var": "error",
+		"object-shorthand": ["error", "always"],
+		"prefer-const": "off",
+		"prefer-template": "error",
+		quotes: ["error", "double", { allowTemplateLiterals: true }],
+		semi: ["error", "always", { omitLastInOneLineBlock: true }],
+		"import/no-anonymous-default-export": "off",
 		"react/no-unknown-property": [
 			"error",
 			{
 				ignore: ["class", "for"]
 			}
-		]
+		],
+		"unicorn/consistent-function-scoping": "off",
+		"unicorn/custom-error-definition": "off",
+		"unicorn/filename-case": [
+			"warn",
+			{
+				cases: {
+					pascalCase: true,
+					kebabCase: true
+				},
+				ignore: ["API", "ID", "IP", "PDF", "URL"]
+			}
+		],
+		"unicorn/no-array-callback-reference": "off",
+		"unicorn/no-await-expression-member": "off",
+		"unicorn/no-keyword-prefix": "off",
+		"unicorn/no-null": "off",
+		"unicorn/no-static-only-class": "off",
+		// TODO: This rule is confusing. What am I meant to fix?
+		"unicorn/no-unsafe-regex": "off",
+		"unicorn/no-useless-undefined": [
+			"error",
+			{
+				checkArguments: false
+			}
+		],
+		"unicorn/prefer-node-protocol": "off",
+		"unicorn/prevent-abbreviations": "off"
 	}
 };
