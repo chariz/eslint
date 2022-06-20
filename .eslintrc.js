4275b517f29a532731344e4492e1fea6ebb828de
Adam Demasi <adam@hbang.com.au>
2022-06-21 01:40:50 +0930

    feat: Add a bunch more codestyle rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,24 +49,64 @@ module.exports = {
 	plugins: ["simple-import-sort"],
 
 	rules: {
+		"array-bracket-newline": ["error", "consistent"],
+		"array-bracket-spacing": ["error", "never"],
 		"array-callback-return": "error",
+		"array-element-newline": ["error", "consistent"],
 		"comma-dangle": ["error", "never"],
+		"dot-location": ["error", "property"],
 		eqeqeq: ["error", "always", { null: "ignore" }],
+		"func-call-spacing": ["error", "never"],
+		"function-call-argument-newline": ["error", "consistent"],
 		indent: ["error", "tab", { SwitchCase: 1, offsetTernaryExpressions: true }],
+		"max-lines-per-function": ["warn", { max: 300 }],
+		"max-params": ["warn", { max: 5 }],
+		"multiline-comment-style": ["error", "separate-lines"],
+		"multiline-ternary": ["error", "always-multiline"],
 		"newline-per-chained-call": ["error", { ignoreChainWithDepth: 1 }],
+		"no-empty": "off",
 		"no-implicit-coercion": "error",
 		// Disabling because this false-positives a lot when using spaces to align things. It’s too
 		// noisy to use an eslint disable comment every time this gets in the way.
 		"no-mixed-spaces-and-tabs": "off",
 		"no-spaced-func": "off",
+		// Has a lot of false positives
 		"no-unused-vars": "off",
 		"no-useless-return": "error",
 		"no-var": "error",
+		"no-warning-comments": "warn",
+		"object-curly-newline": [
+			"error",
+			{
+				multiline: true,
+				consistent: true
+			}
+		],
+		"object-curly-spacing": ["error", "always"],
 		"object-shorthand": ["error", "always"],
+		"one-var": ["error", "never"],
+		"padded-blocks": ["error", "never"],
 		"prefer-const": ["error", { destructuring: "all" }],
 		"prefer-template": "error",
+		"quote-props": ["error", "as-needed"],
 		quotes: ["error", "double", { allowTemplateLiterals: true }],
 		semi: ["error", "always", { omitLastInOneLineBlock: true }],
+		"sort-imports": [
+			"error",
+			{
+				ignoreCase: true,
+				ignoreDeclarationSort: true
+			}
+		],
+		"space-before-function-paren": [
+			"error",
+			{
+				anonymous: "always",
+				asyncArrow: "always",
+				named: "never"
+			}
+		],
+		"spaced-comment": ["error", "always", { markers: ["/", "!", "*"] }],
 		"import/no-anonymous-default-export": "off",
 		"react/no-unknown-property": ["error", { ignore: ["class", "for"] }],
 		"unicorn/consistent-function-scoping": "off",
@@ -86,10 +126,20 @@ module.exports = {
 		"unicorn/no-keyword-prefix": "off",
 		"unicorn/no-null": "off",
 		"unicorn/no-static-only-class": "off",
-		// TODO: This rule is confusing. What am I meant to fix?
+		// This rule is confusing. What am I meant to fix?
 		"unicorn/no-unsafe-regex": "off",
 		"unicorn/no-useless-undefined": ["error", { checkArguments: false }],
 		"unicorn/prefer-node-protocol": "off",
 		"unicorn/prevent-abbreviations": "off"
+	},
+
+	settings: {
+		jest: {
+			version: "28"
+		},
+
+		react: {
+			version: "latest"
+		}
 	}
 };
